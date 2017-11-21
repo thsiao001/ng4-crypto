6f6fa35bf8070bc0178961fbad41f9fe128d5232
Ted Hsiao <thsiao@netskope.com>
2017-11-20 17:21:06 -0800

    Set up simple router. 2 pages so far, home / watchlsit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { Ng2SmartTableModule } from 'ng2-smart-table';
-import {HttpModule} from '@angular/http';
-import {CoinMarketService} from './service/coinMarket.service';
+import { HttpModule } from '@angular/http';
+import { CoinMarketService } from './service/coinMarket.service';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { WatchListComponent } from './watch-list/watch-list.component';
+import { Routes, RouterModule } from '@angular/router';
 
+const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'watchlist', component: WatchListComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    WatchListComponent
   ],
   imports: [
     BrowserModule,
     HttpModule,
-    Ng2SmartTableModule
+    Ng2SmartTableModule,
+    RouterModule.forRoot(routes, {useHash: true})
   ],
   providers: [CoinMarketService],
   bootstrap: [AppComponent]
